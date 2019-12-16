495874231223020b50b7c919540cc79908005982
maryam <mhatami1212@gmail.com>
2019-12-16 06:56:28 -0500

    getting errors for functions in user.h when make qemu

--- getChildrenTest.c
+++ getChildrenTest.c
@@ -10,18 +10,19 @@ int main(int argc, char *argv[]){
 
 int id;
 int childrenID;
-fork();
-fork();
+int fork1 = fork();
+int fork2 = fork();
 
 //getting currnet process ID
 id = getpid(); 
-
+if (fork1 > 0 && fork2 > 0){
 printf(1,"the current  process ID is \n %d\n",getpid());
 printf(1,"the current  process PID is \n %d\n",getppid());
-
+}
+else 
+printf("fork system call was unsuccessful");
 
 childrenID = getChild(id);
-
 printf(1,"parent is ** %d ** and his children are ** %d **\n",id,childrenID);
 
 exit();
